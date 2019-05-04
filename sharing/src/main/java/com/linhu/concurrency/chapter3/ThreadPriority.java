547db894cf2a99acb535e8ef6712481eefb232ef
linhu-pc <1318066698@qq.com>
2019-05-04 18:16:45 +0800

    moniter学习

---
+++ sharing/src/main/java/com/linhu/concurrency/chapter3/ThreadPriority.java
@@ -0,0 +1,23 @@
+package com.linhu.concurrency.chapter3;
+
+public class ThreadPriority {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(()->{
+           while (true){
+               System.out.println("t1");
+           }
+        });
+        t1.setPriority(3);
+
+        Thread t2 = new Thread(()->{
+            while (true){
+                System.out.println("t2");
+            }
+        });
+        t1.setPriority(10);
+
+        t1.start();
+        t2.start();
+    }
+}
